f621b4d0b74d0c3132060561d34616936b8734f8
Fu Xing <bio.xfu@gmail.com>
2018-11-15 14:16:03 +0800

    calculate RPM

--- script/RPKM.R
+++ script/RPKM.R
@@ -6,14 +6,23 @@ samples <- rownames(dfm)
 mapped <- as.numeric(gsub(',', '', sub(' .+', '', dfm$Mapped.reads))) / 1000000
 
 RPKM <- NULL
+RPM <- NULL
+
 for (i in 1:length(samples)) {
   cnt <- read.table(paste0('count/', samples[i], '.cnt'))
   rpkm <- cnt$V3 / mapped[i] / (cnt$V2 / 1000)
   names(rpkm) <- cnt$V1
   RPKM <- cbind(RPKM, rpkm)
+  
+  rpm <- cnt$V3 / mapped[i]
+  names(rpm) <- cnt$V1
+  RPM <- cbind(RPM, rpm)
 }
 
 RPKM <- round(RPKM, 4)
 colnames(RPKM) <- samples
-
 write.table(RPKM, 'table/virus_expression_RPKM.tsv', col.names = NA, sep = '\t', quote = F)
+
+RPM <- round(RPM, 4)
+colnames(RPM) <- samples
+write.table(RPM, 'table/virus_expression_RPM.tsv', col.names = NA, sep = '\t', quote = F)
